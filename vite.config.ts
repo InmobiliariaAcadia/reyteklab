aa240c87dbb4538c27a7b3ed535c1d60ffb093de
Acadia Inmobiliaria <ldy@acadiainmobiliaria.com>
2025-12-19 07:28:27 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      API_KEY: process.env.API_KEY,
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
-    }
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    emptyOutDir: true
   }
 });
